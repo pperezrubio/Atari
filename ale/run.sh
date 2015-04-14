258203aec510dfa5b9317262ac54302df9c1f921
tim <lau.nhtimothy@gmail.com>
2015-04-14 02:13:13 -0400

    added space_invaders and beam_rider and modified Qnn/ale/run.sh

--- ale/run.sh
+++ ale/run.sh
@@ -1,18 +1,21 @@
 RUNLENGTH_ENC=false
 # dont make the name as DISPLAY
 DISPLAY_SCR=true
-FRAMESKIP=8
+FRAMESKIP=1
 
 GAME_CON=fifo_named
-#GAME_CON=internal
+GAME_CON=internal
 
 FOLD=./roms/
 
-GAME=pong.bin
+#GAME=pong.bin
 #GAME=breakout.bin
-#GAME=enduro.bin
-#GAME=qbert.bin
 #GAME=seaquest.bin
+GAME=space_invaders.bin
+#GAME=beam_rider.bin
+
+mkfifo ale_fifo_in
+mkfifo ale_fifo_out
 
 ./ale -game_controller $GAME_CON -player_agent keyboard_agent -run_length_encoding $RUNLENGTH_ENC -display_screen $DISPLAY_SCR -frame_skip $FRAMESKIP ${FOLD}${GAME}
 
