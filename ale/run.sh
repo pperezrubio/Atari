dc7864e970847d5a98825dba791c7d324631b2b4
tim <lau.nhtimothy@gmail.com>
2015-04-16 10:35:53 -0400

    run.sh now contains code to run Ale_client, with commandline arguments to match game, mkdir store

--- ale/run.sh
+++ ale/run.sh
@@ -1,22 +1,28 @@
 RUNLENGTH_ENC=false
-# dont make the name as DISPLAY
-DISPLAY_SCR=true
-FRAMESKIP=1
+# Toggle display output
+DISPLAY_SCR=false
+FRAMESKIP=4
 
+# Playing through pipe
 GAME_CON=fifo_named
-GAME_CON=internal
+# Playing by internal agent (keyboard)
+#GAME_CON=internal
 
 FOLD=./roms/
 
+# Games
 #GAME=pong.bin
-#GAME=breakout.bin
+GAME=breakout.bin
 #GAME=seaquest.bin
-GAME=space_invaders.bin
+#GAME=space_invaders.bin
 #GAME=beam_rider.bin
 
+# make pipes
 mkfifo ale_fifo_in
 mkfifo ale_fifo_out
+# make folder
+mkdir store
 
-./ale -game_controller $GAME_CON -player_agent keyboard_agent -run_length_encoding $RUNLENGTH_ENC -display_screen $DISPLAY_SCR -frame_skip $FRAMESKIP ${FOLD}${GAME}
+./ale -game_controller $GAME_CON -player_agent keyboard_agent -run_length_encoding $RUNLENGTH_ENC -display_screen $DISPLAY_SCR -frame_skip $FRAMESKIP ${FOLD}${GAME} &
 
-
+python Ale_client.py $GAME
